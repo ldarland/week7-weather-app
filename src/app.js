3c24e55b3fc9dd3f68ea37fe01233e3fb06d81c9
ldarland <lindsey@atlascostudios.com>
2023-03-23 13:11:02 -0500

    added content from previous files

---
+++ src/app.js
@@ -0,0 +1,41 @@
+let currentDate = new Date();
+let time = document.querySelector("#current-time");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[currentDate.getDay()];
+let hours = currentDate.getHours();
+let minutes = String(currentDate.getMinutes()).padStart(2, "0");
+
+time.innerHTML = `${day} ${hours}:${minutes}`;
+
+function displayWeather(response) {
+  let location = document.querySelector("#current-city");
+  location.innerHTML = response.data.name;
+
+  let temp = document.querySelector("#temperature");
+  temp.innerHTML = Math.round(response.data.main.temp);
+
+  let humidityElement = document.querySelector("#humid");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+function showCity(event) {
+  event.preventDefault();
+  let apiKey = "2b6fdad0cbd018949c50c70f72250726";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+let citySearch = document.querySelector("#search-city-bar");
+citySearch.addEventListener("submit", showCity);
